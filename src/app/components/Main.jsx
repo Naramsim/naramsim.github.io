b7174e0ac78c889e3072c2ea160ce67c1998c780
Alessandro Pezzè <igougi.ui@gmail.com>
2016-09-18 12:34:54 +0200

    ajax call

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
-import ReactDom from 'react-dom'
+import ReactDom from 'react-dom';
+import Axios from 'axios';
 
 const Main = React.createClass({
+	getInitialState: function() {
+		return {
+			username: '',
+			email: '',
+			location: '',
+		};
+	},
+	componentDidMount: function() {
+		const _this = this;
+		this.serverRequest = Axios.get(this.props.source).then((result) => {
+			console.log(result)
+			var user = result.data;
+			_this.setState({
+				username: user.name,
+				email: user.email,
+				location: user.location,
+			});
+		});
+	},
+	componentWillUnmount: function() {
+		this.serverRequest.abort();
+	},
 	render() {
 		return (
-			<div>Ale</div>
+			<div>
+				<div>{this.state.username}</div>
+				<div>{this.state.email}</div>
+				<div>{this.state.location}</div>
+			</div>
 		);
 	}
 });
 
-ReactDom.render(<Main />, document.getElementById('full'))
+ReactDom.render(
+	<Main source='https://api.github.com/users/naramsim'/>, 
+	document.getElementById('full')
+);
